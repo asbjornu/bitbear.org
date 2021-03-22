fb827acdb12bdd0aaac80c67f2329290d6c18be0
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2021-03-22 13:27:37 +0100

    Use block operator and map to make parsing a bit more terse and elegant

--- _plugins/soundcloud.rb
+++ _plugins/soundcloud.rb
@@ -30,29 +30,30 @@ module Jekyll
         tracks_url = URI::join(base_url, '/bitbear/tracks')
         tracks_source = URI.open(tracks_url)
         tracks_doc = Nokogiri::HTML(tracks_source)
-        tracks = []
-
-        tracks_doc.css('section > article > h2 > a:first-of-type').each do |track_link|
+        tracks_doc.css('section > article > h2 > a:first-of-type').map do |track_link|
           track_name = track_link.content
           track_href = track_link['href']
           track_url = URI::join(base_url, track_href)
           track_source = URI.open(track_url)
           track_doc = Nokogiri::HTML(track_source)
-          track_id = track_doc.css('link[href^="ios-app://"]').map { |l| find_track_id(l) }.first
-
-          tracks.push({
+          track_id = track_doc.css('link[href^="ios-app://"]').map(&:track_id).first
+          {
             :id => track_id,
             :name => track_name,
             :url => track_url
-          })
+          }
         end
-
-        tracks
       end
+    end
+  end
+end
 
-      def find_track_id(ios_link)
+module Nokogiri
+  module XML
+    class Element
+      def track_id
         ios_app_regex = /sounds\:(\d+)/
-        ios_link_href = ios_link['href']
+        ios_link_href = self['href']
         match = ios_app_regex.match(ios_link_href)
         match.captures.first
       end
